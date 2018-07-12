cd3652940fee19f03e9d3f2ac6af657dd47eb2d5
Volodymyr Yahello <vyahello@gmail.com>
2018-07-13 00:00:36 +0300

    Add bot message handler

---
+++ src/bot/message.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from functools import lru_cache
+from typing import Dict, Any, Callable
+from src.server import requests
+
+
+class Answer(ABC):
+    """Abstraction for a answer."""
+
+    @abstractmethod
+    def chat_id(self) -> int:
+        pass
+
+    @abstractmethod
+    def message(self) -> str:
+        pass
+
+
+class Message(ABC):
+    """Abstraction for a message."""
+
+    @abstractmethod
+    def send(self) -> Dict[Any, Any]:
+        pass
+
+
+class BotAnswer(Answer):
+    """An answer from a bot."""
+
+    def __init__(self, request: requests.Request) -> None:
+
+        def _req() -> Dict[Any, Any]:
+            return request.dct().get('message')
+
+        self._req: Callable[..., Dict[Any, Any]] = _req
+
+    def chat_id(self) -> int:
+        return self._req().get('chat').get('id')
+
+    def message(self) -> str:
+        return self._req().get('text')
